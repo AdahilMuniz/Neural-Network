913d247ffe193acc21be01a6f239b51011576231
AdahilMuniz <adahilmuniz@hotmail.com>
2020-04-01 11:43:36 -0300

    Matrix class begin

---
+++ include/Matrix.h
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <omp.h>
+#include <vector>
+#include <new>
+
+using namespace std;
+
+//typedef vector<vector<double>> MatrixArray;
+
+class Matrix
+{
+private:
+    double * values;
+    int nbr, nbc;
+
+public:
+    Matrix(int _nbr, int _nbc, double * _values);
+    Matrix(double * _values);
+    Matrix();
+    Matrix(int _nbr, int _nbc);
+    ~Matrix();
+
+    void setValues(double * _values){ values = _values; };
+    void setDim(int _nbr, int _nbc);
+
+    void print();
+
+    Matrix operator * (Matrix const &m);
+    void   transpose();
+    
+};
+
+/*
+
+int main(int argc, char const *argv[])
+{
+
+    double m_val [][3] = {{1.0,2.0,3.0},
+                     {4.0,5.0,6.0},
+                     {7.0,8.0,9.0},
+                     {7.0,8.0,9.0}};
+
+    Matrix m1(4,3,(double *) m_val);
+    Matrix m2(3,3,(double *) m_val);
+    Matrix m3;
+
+    m3 = m1*m2;
+
+    m3.print();
+
+    m3.transpose();
+    cout << "\n";
+    m3.print();
+
+    return 0;
+}*/
---
+++ src/Matrix.cpp
@@ -0,0 +1,82 @@
+#include "Matrix.h"
+#include <stdio.h>
+
+Matrix::Matrix(int _nbr, int _nbc, double * _values){
+    nbr    = _nbr;
+    nbc    = _nbc;
+    values = _values;
+}
+
+Matrix::Matrix(int _nbr, int _nbc){
+    nbr    = _nbr;
+    nbc    = _nbc;
+    //values = new (nothrow) double [nbr*nbc]; 
+}
+
+Matrix::Matrix(){
+
+}
+
+Matrix::~Matrix(){
+    //
+}
+
+void Matrix::setDim(int _nbr, int _nbc){
+    nbr    = _nbr;
+    nbc    = _nbc;
+    values = new (nothrow) double [nbr*nbc];
+}
+
+Matrix Matrix::operator * (Matrix const &mop){
+    Matrix * mr = new Matrix();
+    double * result = new (nothrow) double [nbr * mop.nbc];
+    int n,m,p;
+
+    if (nbc != mop.nbr){
+        return *mr;
+    }
+    mr = new Matrix(nbr, mop.nbc);
+
+    m = nbr;
+    p = mop.nbc;
+    n = nbc;
+    
+    //double wall_timer = omp_get_wtime();
+    #pragma omp for
+    for (int i = 0; i < m; ++i){
+        for (int j = 0; j < p; ++j){
+            for (int k = 0; k < n; ++k){
+                result[p*i + j] += values[i*n + k]*mop.values[k*p + j];
+            }
+        }
+    }
+    mr->setValues(result);
+
+    //printf("Time to multiply the matrices A(%dx%d) and B(%dx%d): %f\n", m,n,n,p, omp_get_wtime()-  wall_timer);
+
+    return * mr;
+}
+
+void Matrix::transpose(){
+    double * result = new (nothrow) double [nbr * nbc];
+
+    #pragma omp for
+    for (int i = 0; i < nbr; ++i){
+        for (int j = 0; j < nbc; ++j){
+            result[i + j*nbr] = values[j + i*nbc];
+        }
+    }
+    swap(nbr, nbc);
+    values = result;
+}
+
+void Matrix::print(){
+    //cout << "nbr: " << nbr << endl;
+    //cout << "nbc: " << nbc << endl;
+    for (int i = 0; i < nbr; ++i){
+        for (int j = 0; j < nbc; ++j){
+            cout << values[j + i*nbc] << " ";
+        }
+        cout <<"\n";
+    }
+}
